797caabf8ffb5f9b6159bb1ada49df82ae1c491d
SudoCode76 <bo3321319@gmail.com>
2025-04-24 11:35:43 -0400

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../app/components/navbar/navbar.component';
+import { ListProductsComponent } from '../app/components/list-products/list-products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ListProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
