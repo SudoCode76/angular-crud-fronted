353d8a44da9b329f3deb2cb37cab153ef5a85c2d
SudoCode76 <bo3321319@gmail.com>
2025-05-18 18:04:05 -0400

    rutas para inicio y agregar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductsComponent} from './components/add-edit-products/add-edit-products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: ListProductsComponent },
+  { path: 'agregar', component: AddEditProductsComponent },
+  { path: '**', redirectTo: 'inicio', pathMatch: 'full' }, // Redirige a listaproductos si no se encuentra la ruta
+];
