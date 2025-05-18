62b0b55d300d4a2475e23fbf9c7d4c262c8e1d4d
SudoCode76 <bo3321319@gmail.com>
2025-05-18 17:48:19 -0400

    update

---
+++ src/app/components/list-products/list-products.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { Product } from '../../interfaces/product';
+
+@Component({
+  selector: 'app-list-products',
+  imports: [CommonModule],
+  templateUrl: './list-products.component.html',
+  styleUrl: './list-products.component.css'
+})
+export class ListProductsComponent {
+  listProducts : Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Description of Product 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'Description of Product 2',
+      price: 200,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      description: 'Description of Product 3',
+      price: 300,
+    }
+  ]
+}
